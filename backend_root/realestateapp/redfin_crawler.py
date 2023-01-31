471d6f5355d2bf5e74cf910190b989207ee9a775
Shin Ho Jung <jshinhoxp@gmail.com>
2023-01-31 19:45:03 +0200

    added create SQLite tables if not exists

--- backend_root/realestateapp/redfin_crawler.py
+++ backend_root/realestateapp/redfin_crawler.py
@@ -1,4 +1,39 @@
 import sqlite3
 
-conn = sqlite3.connect("housingdata.db")
-cur = conn.cursor
+SQLITE_DB_PATH = "housingdata.db"
+
+def create_tables_if_not_exist():
+   conn = sqlite3.connect(SQLITE_DB_PATH)
+   conn.execute(
+      '''
+      CREATE TABLE IF NOT EXISTS URLS
+      (URL                  TEXT NOT NULL,
+      NUM_PROPOERTIES      INT,
+      NUM_PAGES            INT,
+      PER_PAGES_PROPERTIES INT);
+      '''
+   )
+   conn.execute(
+      '''
+      CREATE TABLE IF NOT EXISTS LISTINGS
+      URL                  TEXT NOT NULL,
+      INFO                 TEXT);
+      '''
+   )
+   conn.execute(
+      '''
+      CREATE TABLE IF NOT EXISTS LISTING_DETAILS(
+      URL            TEXT    NOT NULL,
+      NUMBER_OF_ROOMS INT,
+      NAME           TEXT,
+      COUNTRY        TEXT,
+      REGION         TEXT,
+      LOCALITY       TEXT,
+      STREET         TEXT,
+      POSTAL         TEXT,
+      TYPE           TEXT,
+      PRICE          REAL);
+      '''
+   )
+   conn.close()
+   
