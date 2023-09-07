f6cbb3c34027bf53e7b65c540819cbeb712768b0
Shin Ho Jung <sxjung@uw.edu>
2023-09-07 14:59:19 -0700

    added dataset and algo

--- backend_root/projectApp/views.py
+++ backend_root/projectApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from joblib import dump, load
 # Houses most of the algorithm in background
 
 # class ApprovalsView(viewsets.ModelViewSet):
@@ -8,7 +9,12 @@ from django.http import HttpResponse
 #    serializer_class = ApprovalSerializers
 
 def index(request):
+
    return render(request, "index.html", {})
 
-def about(request):
-   return HttpResponse('<h1>this is about</h1>')
+def prediction(request):
+   # Load
+   sales = pd.read_csv('home_data.csv') 
+   basic_features = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'zipcode']
+   model = joblib.load("basic_model.joblib")
+   y_pred = mdl.predict()
